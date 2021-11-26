b5ee5dcc22d9a46a4e0e8af800bb33e43174c3e1
Teo <teodor.toe@gmail.com>
2021-11-26 02:10:31 +0200

    Structured the code into functions

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 
 const Apify = require('apify');
 const { handleStart, handleList, handleDetail } = require('./src/routes');
-
 const { utils: { log } } = Apify;
 
 Apify.main(async () => {
@@ -18,54 +17,63 @@ Apify.main(async () => {
         handlePageTimeoutSecs: 30,
         maxRequestsPerCrawl: 10,
         maxConcurrency: 50,
-
+ 
         handlePageFunction: async ({request , $ }) => {
           log.debug(`Processing ${request.url}...`)
+         
+         const Product_Title = () =>    // Extracting the title of the product
+         {
+          return $('title').text()
+         }
+         const Product_Price = ()=>
+          {
+            let price
+            let decimal
+            if($('p.product-new-price.has-deal').text())                // Checking for existing prices (discounts or normal)
+              { 
+                price = $('p.product-new-price').clone().children().remove().end().text()       // Extracting the price  without the decimal
+                decimal = $('p.product-new-price').clone().children().text()                    // Extracting the decimal part or the price
+              }
+            if ($('p.product-new-price').text())
+              { 
+                price = $('p.product-new-price').clone().children().remove().end().text()
+                decimal = $('p.product-new-price').clone().children().text()
+              }
+            price=price.split(' ')  // Extracting the first price in the page (the one that corresponds to the product)
+            decimal.split('Lei')    // Extracting the first decimal price
 
-          const title = $('title').text()
-
-          var price;
-          var decimal;
-          
-          if($('p.product-new-price.has-deal').text())
-            { 
-              price = $('p.product-new-price').clone().children().remove().end().text()
-              decimal = $('p.product-new-price').clone().children().text()
-            }
-          else if ($('p.product-new-price').text())
-            { 
-              price = $('p.product-new-price').clone().children().remove().end().text()
-              decimal = $('p.product-new-price').clone().children().text()
-            }
-
-          price = price.split(' ')
-          decimal = decimal.split('Lei')
-          price = `${price[0]},${decimal[0]} Lei`
-      
-          var StockStatus;
-          //Checking if the product is in stock
-          if($('span.label.label-in_stock').text())          
+            return `${price[0]},${decimal[0]}${decimal[1]} Lei`
+          }
+         
+         const Stock_Status = () =>       // Extracting the stock status of the product
+          { 
+            let StockStatus
+            if($('span.label.label-in_stock').text())          
               StockStatus ="InStock"
-          else if($('span.label.label-out_of_stock').text())  
+            if($('span.label.label-out_of_stock').text())  
               StockStatus = "OutOfStock"
-    
-         await Apify.pushData({
-            title, 
-            url: request.url,
-            price,
-            StockStatus,
-        });
-        
-        dataset = await Apify.openDataset();
-        const wholeDataset = await dataset.reduce((memo, value) => {
+          
+            return StockStatus
+          }
+
+          // We push the data into the datasets file
+          await Apify.pushData({        
+            ProductTitle:Product_Title(), 
+            ProductUrl: request.url,
+            ProductPrice:Product_Price(),
+            StockStatus:Stock_Status(),
+          });
+         //We merge the data into the same json file called "output"
+          dataset = await Apify.openDataset();                  
+          const wholeDataset = await dataset.reduce((memo, value) => {
           memo.push(value);
           return memo;
-        }, []);
-        await Apify.setValue('output',wholeDataset);
+          }, []);
+          await Apify.setValue('output',wholeDataset);
         },
     });
 
     log.info('STARTING the crawl.If there are any problems you can stop the crawl with "CTRL+C" ');
     await crawler.run();
-    log.info('Crawler FINISHED. You can now check the OUTPUT.JSON to see the data.');
+    log.info('Crawler FINISHED. You can now check the "output.json" file to see the data.');
 });
